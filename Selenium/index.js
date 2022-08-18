6a5cf2f31196f2497cfa732adaed7ebb02974888
Nikky Amresh <Nikkyamresh8@gmail.com>
2022-08-18 22:47:23 +0530

    Update index.js

--- Selenium/index.js
+++ Selenium/index.js
@@ -4,6 +4,8 @@ const baseUrl = "https://the-internet.herokuapp.com/";
 
 const TheIntenetServices = {
   open: async (driver) => {
+    await driver.get(baseUrl);
+    const title = await driver.getTitle();
     // user needs to open the given URL in the driver and
     // then return the driver and title of the webpage as the object
     return { title, driver };
@@ -12,6 +14,14 @@ const TheIntenetServices = {
     // user needs to open the given URL in the driver and
     // click on the link "File Upload". wait until page is fully loaded and upload the file 'upload.jpg' and submit it
     // then return the driver
+    await driver.get(baseUrl);
+    await driver
+      .findElement(By.xpath("//a[contains(text(),'File Upload')]"))
+      .click();
+    await driver
+      .findElement(By.id("file-upload"))
+      .sendKeys(process.cwd() + "/upload.jpg");
+    await driver.findElement(By.id("file-submit")).click();
 
     return {
       driver,
@@ -19,6 +29,17 @@ const TheIntenetServices = {
   },
 
   changeCheckboxes: async (driver) => {
+    await driver.get(baseUrl);
+    await driver
+      .findElement(By.xpath("//a[contains(text(),'Checkboxes')]"))
+      .click();
+    await driver
+      .findElement(By.xpath('//*[@id="checkboxes"]/input[1]'))
+      .click();
+    await driver
+      .findElement(By.xpath('//*[@id="checkboxes"]/input[2]'))
+      .click();
+
     // open the homepage and click on the "Checkboxes" link
     // check the first checkbox and the uncheck the second checkbox
     // return the driver
